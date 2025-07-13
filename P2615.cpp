9083cfa7d226123db3f0ed805695aa8b9b755072
xiaoyv_404 <xiaoyv_404@outlook.com>
2025-07-14 06:59:48 +0800

    P2615

--- P2615.cpp
+++ P2615.cpp
@@ -3,49 +3,36 @@
 int a[40][40];
 uint n;
 
-int main(){
+int main() {
   std::cin >> n;
 
-  uint pev_x=0, pev_y=n/2;
-  a[pev_x][pev_y]=1;
-  for(size_t i=2; i <= n*n; i++){
-    if(pev_x==0&&pev_y!=n-1){
-      pev_x = n-1;
+  uint pev_x = 0, pev_y = n / 2;
+  a[pev_x][pev_y] = 1;
+  for (size_t i = 2; i <= n * n; i++) {
+    if (pev_x == 0 && pev_y != n - 1) {
+      pev_x = n - 1;
       pev_y++;
-      a[pev_x][pev_y] = i;
-      continue;
-    }
-    if (pev_y==n-1&&pev_x!=0){
+    } else if (pev_y == n - 1 && pev_x != 0) {
       pev_y = 0;
       pev_x--;
-      a[pev_x][pev_y] = i;
-      continue;
-    }
-    if (pev_x==0&&pev_y==n-1){
+    } else if (pev_x == 0 && pev_y == n - 1) {
       pev_x = 1;
-      a[pev_x][pev_y] = i;
-      continue;
-    }
-    if (pev_x!=0&&pev_y!=n-1){
-      int& c = a[pev_x-1][pev_y+1];
-      if (c == 0){
-        c = i;
+    } else if (pev_x != 0 && pev_y != n - 1) {
+      if (a[pev_x - 1][pev_y + 1]) {
         pev_x--;
         pev_y++;
-        continue;
-      }else{
-        a[pev_x+1][pev_y]=i;
+      } else {
         pev_x++;
-        continue;
       }
     }
+    a[pev_x][pev_y] = i;
   }
-  for (size_t x = 0; x < n; x++){
-    for(size_t y = 0; y < n; y++){
+  for (size_t x = 0; x < n; x++) {
+    for (size_t y = 0; y < n; y++) {
       std::cout << a[x][y];
-      if (y==n-1){
+      if (y == n - 1) {
         std::cout << '\n';
-      }else{
+      } else {
         std::cout << ' ';
       }
     }
